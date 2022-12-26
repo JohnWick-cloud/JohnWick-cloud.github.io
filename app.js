3db9bf46a2e7d733bad2c38676f7fdc091287260
JohnWick-cloud <79251228+JohnWick-cloud@users.noreply.github.com>
2022-12-26 22:23:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -90,9 +90,11 @@ Telegram.Webapp.onEvent("mainButtonClicked", function(){
 });
 
 let usercard = document.getElementById("usercard");
+
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}`;
 
-usercard.appendChild(p);
+
+usercard.appendChild(p); 
